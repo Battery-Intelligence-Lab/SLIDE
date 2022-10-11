303805749d685c2dfaae3e9c0b1271771330d04b
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2022-10-11 22:08:39 +0100

    derived_settings.hpp is added

--- src/settings/settings.hpp
+++ src/settings/settings.hpp
@@ -133,18 +133,4 @@ constexpr auto CVcurrentFindingMethod = CVcurrentAlgorithm::falsePosition;
 } // namespace slide::settings
 
 
-namespace slide::settings::data {
-constexpr bool storeCumulativeData = (DATASTORE_CELL >= cellDataStorageLevel::storeCumulativeData);
-constexpr size_t N_CumulativeData = storeCumulativeData ? 3 : 0;
-} // namespace slide::settings::data
-
-
-namespace slide::settings::printBool {
-constexpr auto printCrit = settings::verbose >= printLevel::printCrit;                               //!< threshold of verbose of when to print error messages for critical errors
-constexpr auto printNonCrit = settings::verbose >= printLevel::printNonCrit;                         //!< threshold of verbose of when to print error messages for noncritical errors
-constexpr auto printCyclerFunctions = settings::verbose >= printLevel::printCyclerFunctions;         //!< threshold of verbose of when to print the start and end of functions of the BasicCycler
-constexpr auto printCyclerHighLevel = settings::verbose >= printLevel::printCyclerHighLevel;         //!< threshold of verbose of when to print the high-level flow of the program in the BasicCycler
-constexpr auto printCyclerDetail = settings::verbose >= printLevel::printCyclerDetail;               //!< threshold of verbose of when to print the low-level detailed flow of the program in the BasicCycler
-constexpr auto printfindCVcurrentDetail = settings::verbose >= printLevel::printfindCVcurrentDetail; //!< threshold of verbose of when to print the details of how the current for the CV phase is found
-constexpr auto printCellFunctions = settings::verbose >= printLevel::printCellFunctions;             //!< threshold of verbose of when to print messages at the start and end of functions of the Cell
-};                                                                                                   // namespace slide::settings::printBool
+#include "derived_settings.hpp"
