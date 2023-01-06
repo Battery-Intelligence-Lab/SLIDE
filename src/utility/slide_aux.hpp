ef7f3c1b223319a6dd8718d9f0b4b99d95659311
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2023-01-06 07:50:49 +0000

    some functions are moved in array_util.hpp

--- src/utility/slide_aux.hpp
+++ src/utility/slide_aux.hpp
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include "array_util.hpp"
 #include "interpolation.hpp"
 #include "../types/matrix.hpp"
 #include "../types/XYdata.hpp"
@@ -78,50 +79,3 @@ struct GammaDensityFunctor
 };
 
 } // namespace slide
-
-//!< Array operations to make summing/subtracting arrays easier.
-
-template <typename T, size_t N>
-auto arrSum(const std::array<T, N> &a1, const std::array<T, N> &a2, double b1, double b2)
-{
-  std::array<T, N> c;
-
-  for (size_t i = 0; i < N; i++)
-    c[i] = b1 * a1[i] + b2 * a2[i];
-
-  return c;
-}
-
-template <typename T, size_t N>
-auto operator+=(std::array<T, N> &c, const std::array<T, N> &b)
-{
-  for (size_t i = 0; i < N; i++)
-    c[i] += b[i];
-
-  return c;
-}
-
-template <typename T, size_t N>
-auto operator+(const std::array<T, N> &a, const std::array<T, N> &b)
-{
-  auto c = a;
-  c += b;
-  return c;
-}
-
-template <typename T, size_t N>
-auto operator-=(std::array<T, N> &c, const std::array<T, N> &b)
-{
-  for (size_t i = 0; i < N; i++)
-    c[i] -= b[i];
-
-  return c;
-}
-
-template <typename T, size_t N>
-auto operator-(const std::array<T, N> &a, const std::array<T, N> &b)
-{
-  auto c = a;
-  c -= b;
-  return c;
-}
