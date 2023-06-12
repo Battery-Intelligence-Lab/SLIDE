7c1f67f3924a170f11c0329a0e388b6d41c7b0bc
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2023-06-12 05:39:12 +0100

    Cell_ECM_test.cpp uses Catch2 now.

--- tests/unit/Cell_ECM_test.cpp
+++ tests/unit/Cell_ECM_test.cpp
@@ -5,7 +5,6 @@
  *  Author(s): Jorn Reniers, Volkan Kumtepeli
  */
 
-#include "../tests_util.hpp"
 #include "../../src/slide.hpp"
 
 #include <cassert>
@@ -13,85 +12,75 @@
 #include <fstream>
 #include <span>
 
-namespace slide::tests::unit {
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-bool test_constructor_ECM()
+using Catch::Matchers::WithinAbs;
+using namespace slide;
+
+TEST_CASE("Test ECM constructors", "[ECM]")
 {
   Cell_ECM c1;
-  assert(NEAR(c1.Cap(), 16));
-  assert(NEAR(c1.Vmin(), 2.7));
-  assert(NEAR(c1.Vmax(), 4.2));
-  assert(NEAR(c1.I(), 0));
-  assert(NEAR(c1.getIr(), 0));
-  assert(NEAR(c1.SOC(), 0.5));
-  assert(NEAR(c1.T(), settings::T_ENV));
+  REQUIRE_THAT(c1.Cap(), WithinAbs(16, 1e-15));
+  REQUIRE_THAT(c1.Vmin(), WithinAbs(2.7, 1e-15));
+  REQUIRE_THAT(c1.Vmax(), WithinAbs(4.2, 1e-15));
+  REQUIRE_THAT(c1.I(), WithinAbs(0, 1e-15));
+  REQUIRE_THAT(c1.getIr(), WithinAbs(0, 1e-15));
+  REQUIRE_THAT(c1.SOC(), WithinAbs(0.5, 1e-15));
+  REQUIRE_THAT(c1.T(), WithinAbs(settings::T_ENV, 1e-15));
 
   double soc = 1;
   double cap = 5;
   Cell_ECM c2(cap, soc);
-  assert(NEAR(c2.Cap(), 5));
-  assert(NEAR(c2.Vmin(), 2.7));
-  assert(NEAR(c2.Vmax(), 4.2));
-  assert(NEAR(c2.I(), 0));
-  assert(NEAR(c2.getIr(), 0));
-  assert(NEAR(c2.SOC(), 1));
-  assert(NEAR(c2.T(), settings::T_ENV));
-
-  return true;
+  REQUIRE_THAT(c2.Cap(), WithinAbs(5, 1e-15));
+  REQUIRE_THAT(c2.Vmin(), WithinAbs(2.7, 1e-15));
+  REQUIRE_THAT(c2.Vmax(), WithinAbs(4.2, 1e-15));
+  REQUIRE_THAT(c2.I(), WithinAbs(0, 1e-15));
+  REQUIRE_THAT(c2.getIr(), WithinAbs(0, 1e-15));
+  REQUIRE_THAT(c2.SOC(), WithinAbs(1, 1e-15));
+  REQUIRE_THAT(c2.T(), WithinAbs(settings::T_ENV, 1e-15));
 }
 
-bool test_getStates_ECM()
+TEST_CASE("Test getting ECM states", "[ECM]")
 {
   Cell_ECM c1;
   std::vector<double> s;
 
   c1.getStates(s);
-  assert(NEAR(s[State_ECM<1>::i_SOC], 0.5));           //!< soc
-  assert(NEAR(s[State_ECM<1>::i_Ir], 0));              //!< Ir
-  assert(NEAR(s[State_ECM<1>::i_T], settings::T_ENV)); //!< T
-  assert(NEAR(s[State_ECM<1>::i_I], 0));               //!< current
-
-  std::span<double> spn{ s };
-  c1.setStates(spn); //!< #TODO this must throw an error
-
-  return true;
+  REQUIRE_THAT(s[State_ECM<1>::i_SOC], WithinAbs(0.5, 1e-15));           //!< soc
+  REQUIRE_THAT(s[State_ECM<1>::i_Ir], WithinAbs(0, 1e-15));              //!< Ir
+  REQUIRE_THAT(s[State_ECM<1>::i_T], WithinAbs(settings::T_ENV, 1e-15)); //!< T
+  REQUIRE_THAT(s[State_ECM<1>::i_I], WithinAbs(0, 1e-15));               //!< current
 }
 
-bool test_getV_ECM()
+TEST_CASE("Test ECM getV", "[ECM]")
 {
   //!< double V(bool print = true); //!< crit is an optional argument
   Cell_ECM c1;
 
   //!< normal cell, should give no errors
-  assert(NEAR(c1.V(), 3.15));
-  assert(NEAR(c1.V(), 3.15));
-  assert(NEAR(c1.V(), 3.15));
+  REQUIRE_THAT(c1.V(), WithinAbs(3.15, 1e-15));
+  REQUIRE_THAT(c1.V(), WithinAbs(3.15, 1e-15));
+  REQUIRE_THAT(c1.V(), WithinAbs(3.15, 1e-15));
 
   //!< set to charging and check the voltage has increased
   c1.setCurrent(-1);
   double V = c1.V();
-  assert(V > 3.15);
+  REQUIRE(V > 3.15);
   c1.timeStep_CC(5);
-  assert(c1.V() > V);
+  REQUIRE(c1.V() > V);
 
   //!< set to discharge
   V = c1.V();
   c1.setCurrent(1);
-  assert(c1.V() < V);
+  REQUIRE(c1.V() < V);
   V = c1.V();
   c1.timeStep_CC(5);
-  assert(c1.V() < V);
-
-  // //!< cell with SOC out of range #TODO
-  // Cell_ECM c2(1, 1); //!< make a cell with soC equal to 1
-  // c2.setCurrent(-1, false, false);
-  // c2.timeStep_CC(3600); //!< charge further for one hour, now the SOC should be close to 2
-  // c2.V(false);
-
-  return true;
+  REQUIRE(c1.V() < V);
 }
 
-bool test_setStates_ECM()
+TEST_CASE("Test setting ECM states", "[ECM]")
 {
   Cell_ECM c1;
 
@@ -108,36 +97,14 @@ bool test_setStates_ECM()
 
   s.clear();
   c1.getStates(s);
-  assert(NEAR(s[State_ECM<1>::i_SOC], soc)); //!< soc
-  assert(NEAR(s[State_ECM<1>::i_Ir], ir));   //!< Ir
-  assert(NEAR(s[State_ECM<1>::i_T], t));     //!< T
-  assert(NEAR(s[State_ECM<1>::i_I], i));     //!< current
-
-  // //!< set invalid states #TODO
-  // soc = 2;
-  // t = 0;
-  // s[State_ECM::i_SOC] = soc;
-  // s[State_ECM::i_T] = t;
-  // c1.setStates(spn);
-
-  // //!< set states which violate voltage
-  // soc = 1;
-  // ir = -5;
-  // t = 25_degC;
-  // i = -1;
-  // s[State_ECM::i_SOC] = soc;
-  // s[State_ECM::i_Ir] = ir;
-  // s[State_ECM::i_T] = t;
-  // s[State_ECM::i_I] = i;
-
-  // c1.setStates(spn);
-
-  return true;
+  REQUIRE_THAT(s[State_ECM<1>::i_SOC], WithinAbs(soc, 1e-15)); //!< soc
+  REQUIRE_THAT(s[State_ECM<1>::i_Ir], WithinAbs(ir, 1e-15));   //!< Ir
+  REQUIRE_THAT(s[State_ECM<1>::i_T], WithinAbs(t, 1e-15));     //!< T
+  REQUIRE_THAT(s[State_ECM<1>::i_I], WithinAbs(i, 1e-15));     //!< current
 }
 
-bool test_validStates_ECM()
+TEST_CASE("Test ECM validStates", "[ECM]")
 {
-  //!< bool validStates(double s[], int nin);
   Cell_ECM c1;
 
   //!< set valid new states
@@ -149,67 +116,23 @@ bool test_validStates_ECM()
   s[State_ECM<1>::i_I] = i;
   std::span<double> spn{ s };
   c1.setStates(spn);
-  assert(c1.validStates());
-
-  //!< set invalid states
-  // s[State_ECM::i_SOC] = 2; //!< soc
-  // c1.setStates(spn);
-
-  // assert(!c1.validStates());
-
-  // c1.setStates(spn);
-  // s[State_ECM::i_SOC] = 0.5; //!< soc
-  // s[State_ECM::i_T] = 0;     //!< T
-  // assert(!c1.validStates());
-
-  return true;
+  REQUIRE(c1.validStates());
 }
 
-bool test_timeStep_CC_ECM()
+TEST_CASE("Test ECM timeStep_CC", "[ECM]")
 {
-  //!< bool timeStep_CC(double dt);
-  Cell_ECM c1;
-  double I = -1;
-  double dt = 5;
-  double tol = 0.002;
+  constexpr double I = -1;
+  constexpr double dt = 5;
+  constexpr double tol = 0.002;
 
+  Cell_ECM c1;
   //!< soc initial = 0.5 and capacity = 10
   //!< so SOC_end = 0.5 + 1*5/3600 = 0.5014
   c1.setCurrent(I);
   c1.timeStep_CC(dt);
-  double err = c1.SOC() - 0.5014;
-  assert(err < tol && err > -tol);
+  REQUIRE_THAT(c1.SOC(), WithinAbs(0.5014, tol));
 
   c1.setCurrent(-I);
   c1.timeStep_CC(dt);
-  err = c1.SOC() - 0.50;
-  assert(err < tol && err > -tol);
-
-  return true;
+  REQUIRE_THAT(c1.SOC(), WithinAbs(0.50, tol));
 }
-
-int test_all_Cell_ECM()
-{
-  /*
-   * calls all test-functions
-   *
-   * IN
-   *  	if true, we will also verify that things which have to go wrong, actually do go wrong
-   * 					this will result in error messages being printed to the terminal
-   * 					all the errors are caught, so the code should not crash
-   * 				if false, we only test things which should go well
-   */
-
-  //!< if we test the errors, suppress error messages
-  if (!TEST(test_constructor_ECM, "test_constructor_ECM")) return 1;
-  if (!TEST(test_getStates_ECM, "test_getStates_ECM")) return 2;
-  if (!TEST(test_getV_ECM, "test_getV_ECM")) return 3;
-  if (!TEST(test_setStates_ECM, "test_setStates_ECM")) return 4;
-  if (!TEST(test_validStates_ECM, "test_validStates_ECM")) return 5;
-  if (!TEST(test_timeStep_CC_ECM, "test_timeStep_CC_ECM")) return 6;
-
-  return 0;
-}
-} // namespace slide::tests::unit
-
-int main() { return slide::tests::unit::test_all_Cell_ECM(); }
