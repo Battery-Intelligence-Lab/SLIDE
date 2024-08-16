b840ed3858d2dfd46b9c07edb29bf6ba79c069c2
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2024-08-16 17:36:41 +0100

    Deep_ptr now inherits from std::unique_ptr<T>

--- src/types/Deep_ptr.hpp
+++ src/types/Deep_ptr.hpp
@@ -14,101 +14,42 @@
 #pragma once
 
 #include <memory>
+#include <type_traits> /// For std::is_base_of
 
 namespace slide {
 
 template <typename T>
-class Deep_ptr
+class Deep_ptr : public std::unique_ptr<T>
 {
-  std::unique_ptr<T> ptr{};
-
 public:
-  Deep_ptr() = default;               //!< Default constructor
-  explicit Deep_ptr(T *p) : ptr(p) {} //!< Constructor taking a raw pointer
-
-  Deep_ptr(std::unique_ptr<T> &&uptr) : ptr(std::move(uptr)) {} // Constructor taking a unique_ptr
+  Deep_ptr() = default;                                                           /// Default constructor
+  explicit Deep_ptr(T *p) noexcept : std::unique_ptr<T>{ p } {}                   /// Constructor taking a raw pointer
+  Deep_ptr(Deep_ptr &&other) noexcept : std::unique_ptr<T>{ std::move(other) } {} /// Move constructor
 
   /// Copy constructor
-  Deep_ptr(const Deep_ptr &other) : ptr(other ? other->copy() : nullptr) {}
+  Deep_ptr(const Deep_ptr &other) : std::unique_ptr<T>{ bool(other) ? other->copy() : nullptr } {}
 
-  // Templated copy constructor for implicit conversion from derived class.
+  /// Templated copy constructor for implicit conversion from derived class.
   template <typename U, typename = std::enable_if_t<std::is_base_of<T, U>::value>>
-  Deep_ptr(const Deep_ptr<U> &other) : ptr(other ? other->copy() : nullptr) {}
+  Deep_ptr(const Deep_ptr<U> &other) : std::unique_ptr<T>{ bool(other) ? other->copy() : nullptr } {}
 
-  // Templated constructor for implicit conversion from derived class for unique_ptr.
-  template <typename U, typename = std::enable_if_t<std::is_base_of<T, U>::value>>
-  Deep_ptr(const std::unique_ptr<U> &other) : ptr(other ? other->copy() : nullptr) {}
 
-  /// Move constructor
-  Deep_ptr(Deep_ptr &&other) noexcept : ptr(std::move(other.ptr)) {}
-
-  // Templated move constructor for implicit conversion
-  template <typename U, typename = std::enable_if_t<std::is_base_of<T, U>::value>>
-  Deep_ptr(Deep_ptr<U> &&other) noexcept : ptr(std::move(other.ptr)) {}
-
-  // // Templated constructor for implicit conversion from raw pointers
-  // template <typename U, typename = std::enable_if_t<std::is_base_of<T, U>::value>>
-  // explicit Deep_ptr(U *raw_ptr) : ptr(raw_ptr) {}
-
-  // Declare the templated Deep_ptr class as a friend
-  template <typename U>
-  friend class Deep_ptr;
+  Deep_ptr(std::unique_ptr<T> &&up) : std::unique_ptr<T>{ std::move(up) } {}
+  Deep_ptr &operator=(const Deep_ptr &rhs)
+  {
+    std::unique_ptr<T>::reset(Deep_ptr{ rhs }.std::unique_ptr<T>::release());
+    return *this;
+  }
 
   /// Copy assignment operator
-  Deep_ptr &operator=(const Deep_ptr &other)
+  Deep_ptr &operator=(Deep_ptr &&rhs) noexcept
   {
-    if (this != &other) ptr = other ? other->copy() : nullptr;
-
+    std::unique_ptr<T>::reset(rhs.release());
     return *this;
   }
 
-  /// Move assignment operator
-  Deep_ptr &operator=(Deep_ptr &&other) noexcept
-  {
-    if (this != &other) ptr = std::move(other.ptr);
-
-    return *this;
-  }
-
-  T *operator->() const { return ptr.operator->(); } //!< Accessor operator
-  T &operator*() const { return *ptr; }              //!< Dereference operator
-  T *get() const { return ptr.get(); }               //!< Get the raw pointer
-
-  /// Check if the Deep_ptr is not empty
-  explicit operator bool() const { return static_cast<bool>(ptr); }
-  void reset(T *p = nullptr) { ptr.reset(p); } //!< Reset the Deep_ptr
-
-  // Swap the Deep_ptr with another Deep_ptr
-  void swap(Deep_ptr &other) { ptr.swap(other.ptr); }
-
-  // Comparison operators
-  [[nodiscard]] friend bool operator==(const Deep_ptr<T> &lhs, std::nullptr_t) noexcept
-  {
-    return lhs.ptr == nullptr;
-  }
-
-  [[nodiscard]] friend bool operator==(std::nullptr_t, const Deep_ptr<T> &rhs) noexcept
-  {
-    return rhs == nullptr;
-  }
-
-  [[nodiscard]] friend bool operator!=(const Deep_ptr<T> &lhs, std::nullptr_t) noexcept
-  {
-    return !(lhs == nullptr);
-  }
-
-  [[nodiscard]] friend bool operator!=(std::nullptr_t, const Deep_ptr<T> &rhs) noexcept
-  {
-    return rhs != nullptr;
-  }
+  ~Deep_ptr() = default;
 };
-
-// Free functions:
-template <typename T, typename... Args>
-Deep_ptr<T> make_Deep_ptr(Args &&...args)
-{
-  return Deep_ptr<T>(new T(std::forward<Args>(args)...));
-}
 
 /// @brief  Makes storage units.
 /// @tparam T
